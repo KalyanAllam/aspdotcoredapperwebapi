4f61fbbcb589719ef4ba37ae7e9d52f5deee4739
KALYAN <allam.kalyana@outlook.com>
2022-09-17 14:14:34 +0530

    Add project files.

---
+++ aspdotcoredapperwebapi/Contracts/ICompanyRepository.cs
@@ -0,0 +1,4 @@
+﻿public interface ICompanyRepository
+{
+    public Task<IEnumerable<Company>> GetCompanies();
+}
---
+++ aspdotcoredapperwebapi/Repository/CompanyRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+
+public class CompanyRepository : ICompanyRepository
+{
+    private readonly DapperContext _context;
+    public CompanyRepository(DapperContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<IEnumerable<Company>> GetCompanies()
+    {
+        var query = "SELECT * FROM Companies";
+        using (var connection = _context.CreateConnection())
+        {
+            var companies = await connection.QueryAsync<Company>(query);
+            return companies.ToList();
+        }
+    }
+}
+
+
